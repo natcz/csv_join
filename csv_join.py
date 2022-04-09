c3521814fc0cb005d6224fc879decdc377aff876
natcz <nataliaczerep@gmail.com>
2022-04-09 21:03:14 +0200

    added test for correctness of args

--- csv_join.py
+++ csv_join.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import sys
 import argparse
 
 def get_args_parser():
@@ -21,7 +21,7 @@ def get_args_parser():
     
     parser.add_argument(
         "--primarykey_second",
-        help="primary key in the first file",
+        help="primary key in the second file",
     )
     
     parser.add_argument(
@@ -43,14 +43,11 @@ def left_join(file1, file2, pk1, pk2):
         pk2 (str): second file table primary key
     """
     header1 = list(file1.columns.values)
-    header2 = list(file1.columns.values)
+    header2 = list(file2.columns.values)
 
-   
     pk1_column = header1.index(pk1)
     pk2_column = header2.index(pk2)
-
-
-            
+   
     for row in file1:
         print(list(row),end=", ")
         file1_pk_val = row[pk1_column]
@@ -71,14 +68,12 @@ def right_join(file1, file2, pk1, pk2):
         pk2 (str): second file table primary key
     """
     header1 = list(file1.columns.values)
-    header2 = list(file1.columns.values)
+    header2 = list(file2.columns.values)
 
    
     pk1_column = header1.index(pk1)
     pk2_column = header2.index(pk2)
-
-
-            
+           
     for row in file2:
         
         file2_pk_val = row[pk2_column]
@@ -101,8 +96,7 @@ def inner_join(file1, file2, pk1, pk2):
         pk2 (str): second file table primary key
     """
     header1 = list(file1.columns.values)
-    header2 = list(file1.columns.values)
-
+    header2 = list(file2.columns.values)
    
     pk1_column = header1.index(pk1)
     pk2_column = header2.index(pk2)
@@ -117,7 +111,7 @@ def inner_join(file1, file2, pk1, pk2):
             print(list(file1.loc[[index]]),end=", ")
             print(list(row)) 
                       
-            
+        
     
 
 
@@ -129,18 +123,50 @@ if __name__ == "__main__":
     file1 = args.path_first
     file2 = args.path_second
     
-    file1 = pd.pd.read_csv(file1)
-    file2 = pd.pd.read_csv(file2)
-    join_type = args.join_type
+    if not file1.exists():
+        print("Path to the first file is incorrect!")
+        sys.exit()
+    elif not file1.isfile():
+        print("Path to the first file is incorrect!")
+        sys.exit()
     
-    pk1 = args.primarykey_first
-    pk2 = args.primarykey_second
+    if not file2.exists():
+        print("Path to the second file is incorrect!")
+        sys.exit()
+    elif not file2.isfile():
+        print("Path to the second file is incorrect!")
+        sys.exit()
     
-    if join_type == "left":
-        left_join(file1, file2, pk1, pk2)
-    elif join_type == "right":
-        right_join(file1, file2, pk1, pk2)
-    else:
-        inner_join(file1, file2, pk1, pk2)
+    
+    try:
+        file1 = pd.pd.read_csv(file1)
+        file2 = pd.pd.read_csv(file2)
+        
+        join_type = args.join_type
+        if join_type not in ["left", "right", "inner"]:
+            print("Wrong join type, should be right, inner or left!")
+            sys.exit()
+            
+        pk1 = args.primarykey_first
+        pk2 = args.primarykey_second
+        
+        header1 = list(file1.columns.values)
+        header2 = list(file2.columns.values)
+        
+        if pk1 not in header1 or pk2 not in header2:
+            print("Wrong primary keys")
+            sys.exit()
+        
+        if join_type == "left":
+            left_join(file1, file2, pk1, pk2)
+        elif join_type == "right":
+            right_join(file1, file2, pk1, pk2)
+        else:
+            inner_join(file1, file2, pk1, pk2)      
+    finally:
+        print("Cannot read files of given paths!")
+        
+    
+    
         
     
